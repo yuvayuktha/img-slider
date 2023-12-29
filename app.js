5d57fed61c5310162dfee0752cc788e16b96746b
yuva <154859993+yuvayuktha@users.noreply.github.com>
2023-12-29 05:51:04 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+const container = document.querySelector ('.container');
+const btns = document.querySelectorAll('.btn')
+
+const imgList = ["1","2","3","4","5"]
+
+let index=0;
+
+btns.forEach(
+    (button)=>{
+    button.addEventListener('click',()=>{
+        
+        if(button.classList.contains('btn-left')){
+            index--;
+            if(index<0){
+                index = imgList.length-1;
+            }
+            container.style.background = `url("./${imgList[index]}.jpg") center/cover fixed no-repeat`
+        }
+        else{
+            index++;
+            if(index===imgList.length){
+                index = 0;
+            }
+            container.style.background = `url("./${imgList[index]}.jpg") center/cover fixed no-repeat`
+        }
+    })
+}
+)
